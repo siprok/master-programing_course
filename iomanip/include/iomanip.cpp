893b65c1584022decf198f9b8ecf4f007cfdba96
Stepan Prokudin <siprokudin1@mail.ru>
2020-09-20 00:18:58 +0300

    first attempt to perform laboratory work

---
+++ iomanip/include/iomanip.cpp
@@ -0,0 +1,69 @@
+#include<iomanip.hpp>
+
+#include<string>
+
+// endm manipulator part
+std::ostream &endm(std::ostream &arg_ss)
+{
+	arg_ss << "[eol]\n";;
+	arg_ss.flush();
+	return arg_ss;
+}
+
+
+// squares manipulator part
+SQ_proxy::SQ_proxy(std::ostream &source_stream)
+{
+	stream = &source_stream;
+}
+
+SQ_proxy &squares(std::ostream &source_stream)
+{
+	SQ_proxy some_obj(source_stream);
+	return some_obj;
+}
+
+SQ_proxy& operator<<(std::ostream &stream, SQ_proxy& (*manip)(std::ostream&))
+{
+	return manip(stream);
+}
+
+
+// add manipulator part
+ADD_proxy::ADD_proxy(std::ostream &source_stream)
+{
+	head = this;
+	stream = &source_stream;
+}
+
+ADD_proxy::ADD_proxy(ADD_proxy &source_proxy)
+{
+	this->head = source_proxy.head;
+	this->stream = source_proxy.stream;
+}
+
+ADD_proxy* ADD_proxy::get_head()
+{
+	return this->head;
+}
+
+std::ostream& ADD_proxy::get_stream()
+{
+	return *(this->stream);
+}
+
+std::ostream* ADD_proxy::get_stream_ptr()
+{
+	return this->stream;
+}
+
+ADD_proxy *add(std::ostream &source_stream)
+{
+	return new ADD_proxy(source_stream);
+}
+
+ADD_proxy& operator<<(std::ostream &stream, ADD_proxy* (*manip)(std::ostream&))
+{
+	return *(manip(stream));
+}
+
--- iomanip/include/iomanip.hpp
+++ iomanip/include/iomanip.hpp
@@ -3,21 +3,130 @@
  * @date 2018-08-05
  * @author Anonymous
  */
-
+#pragma once
 #ifndef __IOMANIP_HPP__
 #define __IOMANIP_HPP__
 
 #include <iostream>
 
 //{ endm manipulator
-
+std::ostream &endm(std::ostream &arg_ss);
 //}
 
 //{ squares manipulator
+class SQ_proxy 
+{
+private:
+	std::ostream *stream;
+public:
+	SQ_proxy(std::ostream&);
+	
+	friend SQ_proxy& operator<<(std::ostream &stream, SQ_proxy& (*manip)(std::ostream&));
 
+	template<typename T>
+	friend std::ostream& operator<<(SQ_proxy& sq_proxy, const T &mes)
+	{
+		return  *(sq_proxy.stream) << "[" << mes << "]";
+	}
+
+	template<typename T>
+	friend std::ostream& operator<<(SQ_proxy& sq_proxy, T &mes)
+	{
+		return  *(sq_proxy.stream) << "[" << mes << "]";
+	}
+};
+SQ_proxy &squares(std::ostream&);
 //}
 
 //{ add manipulator
+
+template<typename T>
+class ADD_oper_proxy;
+
+class ADD_proxy
+{
+private:
+	ADD_proxy *head;
+	std::ostream *stream;
+
+public:
+	ADD_proxy(std::ostream&);
+	ADD_proxy(ADD_proxy &source_proxy);
+	ADD_proxy(const ADD_proxy &source_proxy);
+
+	ADD_proxy *get_head();
+	std::ostream &get_stream();
+	std::ostream *get_stream_ptr();
+
+	template<typename T>
+	friend ADD_oper_proxy<T>& operator<<(ADD_proxy &proxy, const T &operand)
+	{
+		return *(new ADD_oper_proxy<T>(proxy, operand));
+	}
+
+	template<typename T>
+	friend ADD_oper_proxy<T>& operator<<(ADD_proxy &proxy, T &operand)
+	{
+		return *(new ADD_oper_proxy<T>(proxy, operand));
+	}
+};
+
+ADD_proxy *add(std::ostream&);
+
+ADD_proxy& operator<<(std::ostream &stream, ADD_proxy* (*manip)(std::ostream&));
+
+
+template<typename T>
+class ADD_oper_proxy
+{
+private:
+	ADD_oper_proxy<T> *head;
+	ADD_proxy *base_proxy;
+	T accumulator;
+public:
+	ADD_oper_proxy<T>(ADD_proxy &source_proxy, const T &operand)
+	{
+		this->head = this;
+		this->base_proxy = source_proxy.get_head();
+		accumulator = operand;
+	}
+	
+	ADD_oper_proxy<T>(ADD_proxy &source_proxy, T &operand)
+	{
+		this->head = source_proxy.head;
+		this->base_proxy = source_proxy.get_head();
+		accumulator = operand;
+	}
+
+	ADD_oper_proxy<T> *get_head()
+	{
+		return this->head;
+	}
+
+	ADD_proxy *get_base_proxy_ptr()
+	{
+		return this->base_proxy;
+	}
+
+	friend std::ostream& operator<<(ADD_oper_proxy<T> &oper_proxy, const T &operand)
+	{
+		T buf = oper_proxy.accumulator + operand;
+		std::ostream *stream_buf = oper_proxy.get_base_proxy_ptr()->get_stream_ptr();
+		delete oper_proxy.get_base_proxy_ptr()->get_head();
+		delete oper_proxy.get_head();
+		return *stream_buf << buf;
+	}
+
+	friend std::ostream& operator<<(ADD_oper_proxy<T> &oper_proxy, T &operand)
+	{
+		T buf = oper_proxy.accumulator + operand;
+		std::ostream *stream_buf = oper_proxy.get_base_proxy_ptr()->get_stream_ptr();
+		delete oper_proxy.get_base_proxy_ptr()->get_head();
+		delete oper_proxy.get_head();
+		return *stream_buf << buf;
+	}
+};
+
 
 //}
 
