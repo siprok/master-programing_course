d1d4bb09e0274b992fee573bef67488f13ae28da
Stepan Prokudin <siprokudin1@mail.ru>
2020-11-13 17:43:22 +0300

    lab_variant

--- variant/include/variant.hpp
+++ variant/include/variant.hpp
@@ -15,50 +15,64 @@
 #include <boost/variant.hpp>
 
 //{ number
-struct number
-{
-
-};
+using number = std::variant<int, float>;
 //}
 
 //{ array
-struct array
-{
-
-};
+using array = std::vector<number>;
 //}
 
 //{ recursive_array
-struct recursive_array
+struct recursive_array : public std::vector<std::variant<number, std::shared_ptr<recursive_array>>>
 {
-
+	using std::vector<std::variant<number, std::shared_ptr<recursive_array>>>::vector;
 };
 //}
 
 //{ recursive_array2
-struct recursive_array2
+struct recursive_array2 : public std::vector<std::variant<number, boost::recursive_wrapper<recursive_array2>>>
 {
-
+	recursive_array2(std::initializer_list<std::variant<number, boost::recursive_wrapper<recursive_array2>>> init)
+	{
+		for (const std::variant<number, boost::recursive_wrapper<recursive_array2>>& elem : init)
+			this->push_back(elem);
+	}
 };
 //}
 
 //{ variant_decorator
-template<???>
-struct variant_decorator
+template<typename ... Types>
+struct variant_decorator : public std::variant<Types...>
 {
+	template<class T>
+	variant_decorator(T value) : std::variant<Types...>(value) {}
+	variant_decorator() : std::variant<Types...>() {}
+	template<typename T>
+	T& as()
+	{
+		if constexpr (std::disjunction<std::is_same<Types, T>...>::value)
+			return std::get<T>(*this);
+		else
+			return std::get<boost::recursive_wrapper<T>>(*this).get();
+	}
 
-    ?? as()
-    {
-
-    }
+	template<typename T>
+	T as() const
+	{
+		return std::get<T>(*this);
+	}
 };
+
 //}
 
 //{ recursive_map
-struct recursive_map
-{
 
+struct recursive_map : public std::map<std::string, variant_decorator<int, std::string, bool, boost::recursive_wrapper<recursive_map>>>
+{
+	recursive_map(std::initializer_list<value_type> init) : std::map<std::string, variant_decorator<int, std::string, bool, boost::recursive_wrapper<recursive_map>>>(init) {}
 };
+
 //}
 
-#endif // __VARIANT_HPP__
+#endif 
+// __VARIANT_HPP__
