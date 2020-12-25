cdd70f3473230f0a209ef7859f79e6c60fc3c6cd
Stepan Prokudin <siprokudin1@mail.ru>
2020-12-25 11:43:50 +0300

    type_map lab

--- type_map/include/type_map.hpp
+++ type_map/include/type_map.hpp
@@ -12,47 +12,52 @@
 
 namespace detail
 {
-    template<class T, class Tuple>
-    struct type_to_index;
+	template<class T, class Tuple>
+	struct type_to_index;
 
-    template<class T, class... Ts>
-    struct type_to_index<T, std::tuple<T, Ts...>>
-    {
-        constexpr static size_t v = 0;
-    };
+	template<class T, class... Ts>
+	struct type_to_index<T, std::tuple<T, Ts...>>
+	{
+		constexpr static size_t v = 0;
+	};
 
-    template<class T, class U, class... Ts>
-    struct type_to_index<T, std::tuple<U, Ts...>>
-    {
-        constexpr static size_t v = 1 + type_to_index<T, std::tuple<Ts...>>::v;
-    };
+	template<class T, class U, class... Ts>
+	struct type_to_index<T, std::tuple<U, Ts...>>
+	{
+		constexpr static size_t v = 1 + type_to_index<T, std::tuple<Ts...>>::v;
+	};
 }
 
 //? What is wrong with ``*this[i]``?
 //{
-template<???>
-class type_map: public ???
+template<class T, class ...Args>
+class type_map : public std::array<T, sizeof...(Args)>
 {
 public:
-    using types = ...
+	using types = std::tuple<Args...>;
 
-    template<class T>
-    ?? as()
-    {
-        return ...
-    }
+	template<class U>
+	auto as() const
+	{
+		return this->at(detail::type_to_index<U, types>::v);
+	}
+	template<class U>
+	decltype(auto) as()
+	{
+		return this->at(detail::type_to_index<U, types>::v);
+	}
 };
 //}
 
 namespace std
 {
-    template<typename T, class TypeMap>
-    ?? get(??TypeMap?? tm)
-    {
-        //{ How to call ``as``?
-        return ???
-        //}
-    }
+	template<typename T, class TypeMap>
+	decltype(auto) get(TypeMap& tm)
+	{
+		//{ How to call ``as``?
+		return tm.template as<T>();
+		//}
+	}
 }
 
 #endif // __TYPE_MAP_HPP__
